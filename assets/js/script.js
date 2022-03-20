125e839059145697371a64f926bd5e8f4c8aaff9
jjakobsons87 <98094445+jjakobsons87@users.noreply.github.com>
2022-03-20 15:52:37 -0400

    working API call for city and full dataset

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,32 +1,43 @@
 const apiKey = "c2f4ea3c03d6375b941699ac5edb328c";
 var cityInputEl = document.querySelector("#city-input");
+var putElement = document.querySelector("#input");
 
 function formSubmitHandler(event) {
     event.preventDefault();
-}
+    var city = cityInputEl.value.trim();
+    console.log(city);
+    getWeather(city);
+    cityInputEl.value = "";
+};
 
 
 var getWeather = function (city) {
     // get the city long/lat
-    var cityLoc = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey;
+    var cityLoc =
+        "http://api.openweathermap.org/geo/1.0/direct?q=" + city + "&limit=1&appid=" + apiKey;
     fetch(cityLoc).then(function(response) {
         if(response.ok) {
-            console.log(data);
+            response.json().then(function(data) {
+                console.log(data);
+                //  pull out and define the lon/lat for the city
+                var lat = data[0].lat;
+                var lon = data[0].lon;
+
+                // now take lon/lat and get full forecast 
+                var apiUrl = 
+                "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&units=imperial&exclude=minutely&appid=" + apiKey;
+                console.log(apiUrl);
+                fetch(apiUrl).then(function(response) {
+                    if (response.ok) {
+                        response.json().then(function(data) {
+                            console.log(data);
+                        });
+                    // } else {
+                    //     alert("Error Try again");
+                    }
+                });
+            })
         }
     })
-    
-    // var apiUrl = 
-    // "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&exclude=minutely&appid=" + apiKey;
-    // console.log(apiUrl);
-    // fetch(apiUrl).then(function(response) {
-    //     if (response.ok) {
-    //         response.json().then(function(data) {
-    //             console.log(data);
-    //         });
-    //     } else {
-    //         alert("Error Try again");
-    //     }
-    // });
 };
-
-cityInputEl.addEventListener("submit")
+putElement.addEventListener("submit", formSubmitHandler);
