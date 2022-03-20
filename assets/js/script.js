6faa1f6ada10b7d98fd31f6161904c87e2bd1966
jjakobsons87 <98094445+jjakobsons87@users.noreply.github.com>
2022-03-20 19:44:22 -0400

    start adding forecast call

--- assets/js/script.js
+++ assets/js/script.js
@@ -59,19 +59,20 @@ function displayWeather (data, city) {
         ": " + moment(data.current.dt.value).format("MMM D, YYYY");
     cityDetailsEL.appendChild(currentDate);
 
-    var weatherIcon = document.createElement("img")
-    var icon = data.current.weather[0].icon;
-    console.log(icon);
-    weatherIcon.setAttribute(
-        "src", 'http://openweathermap.org/img/wn/' + icon + '@2x.png');
-    cityInputEl.appendChild(weatherIcon);
-    console.log(weatherIcon);
-
     var tempEl = document.createElement("span");
     tempEl.textContent = "Temperature: " + Math.round(data.current.temp) + " °F";
     tempEl.classList = "list-group-item";
     currentWeatherEl.appendChild(tempEl);
     console.log(data.current.temp);
+
+    var weatherIcon = document.createElement("img")
+    var icon = data.current.weather[0].icon;
+    console.log(icon);
+    weatherIcon.setAttribute(
+        "src", 'http://openweathermap.org/img/wn/' + icon + '@2x.png');
+    weatherIcon.classList = "list-group-item";
+    cityDetailsEL.appendChild(weatherIcon);
+    console.log(weatherIcon);
 
     var humEl = document.createElement("span");
     humEl.textContent = "Humidity: " + data.current.humidity + "%";
@@ -85,8 +86,35 @@ function displayWeather (data, city) {
 
     var uvEl = document.createElement("span");
     uvEl.textContent = "UV: " + data.current.uvi;
-    uvEl.classList = "list-group-item";
+    uvEl.classList = "list-group-item uv";
     currentWeatherEl.appendChild(uvEl);
+
+    forecast(); 
+};
+
+// function to produce 5 day forecast 
+function forecast (data, city) {
+    for (var i = 1; i < 6; i++) {
+        var forecastDay = document.createElement("div")
+        forecastDay.className = ("card col-2 m-2");
+        var forecastDate = document.createElement("h4");
+        forecastDate.textContent = moment(data.daily[1].dt.value).format("MMMM D, YYYY");
+        console.log(data.daily[0].dt.value);
+        // forecastDate.classList = "card-header";
+        // forecastDay.appendChild(forecastDate);
+        // var forecastIcon = document.createElement("img");
+    
+        var forecastTemp = document.createElement("p");
+        forecastTemp.textContent = "Temp: " + data.daily[i].temp + "°F";
+        forecastDate.appendChild(forecastTemp);
+
+        var forecastWind = document.createElement("p");
+        forecastWind.textContent = "Wind: " + data.daily[i].wind_speed + " MPH";
+        forecastDate.appendChild(forecastWind);
+        var forecastHum = document.createElement("p");
+        forecastHum.textContent = "Humidity: " + data.daily[i].humidity + " %";
+        forecastDate.appendChild(forecastHum);
+    }
 };
 
 
