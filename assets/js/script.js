f31cb37fa02fd71d9d96d1ba27fbe8542de2f343
jjakobsons87 <98094445+jjakobsons87@users.noreply.github.com>
2022-03-20 18:32:10 -0400

    add current weather conditions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,13 @@
 const apiKey = "c2f4ea3c03d6375b941699ac5edb328c";
 var cityInputEl = document.querySelector("#city-input");
 var putElement = document.querySelector("#input");
+var currentWeatherEl = document.querySelector("#current-weather");
+var cityDetailsEL = document.querySelector("#city-details");
 // var currentDate = document.getElementById("#current-date");
-var currentTemp = document.querySelector("#current-temp");
-var currentWind = document.querySelector("#current-wind");
-var currentHum = document.querySelector("#current-hum");
-var currentUV = document.querySelector("#current-uv");
+// var currentTemp = document.querySelector("#current-temp");
+// var currentWind = document.querySelector("#current-wind");
+// var currentHum = document.querySelector("#current-hum");
+// var currentUV = document.querySelector("#current-uv");
 
 function formSubmitHandler(event) {
     event.preventDefault();
@@ -30,12 +32,13 @@ var getWeather = function (city) {
                 
                 // now take lon/lat and get full forecast 
                 var apiUrl = 
-                "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&units=imperial&exclude=minutely&appid=" + apiKey;
+                "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&units=imperial&exclude=minutely,hourly&appid=" + apiKey;
                 console.log(apiUrl);
                 fetch(apiUrl).then(function(response) {
                     if (response.ok) {
                         response.json().then(function(data) {
                             console.log(data);
+                            displayWeather(data, city);
                         });
                     } else {
                         alert("Error: Please Try again");
@@ -47,7 +50,44 @@ var getWeather = function (city) {
 };
 
 // once we get the data, fill in the page
-function displayWeather ()
+function displayWeather (data, city) {
+  // currentWeatherEl.textContent = "";
+    cityDetailsEL.textContent = city;
+
+    var currentDate = document.createElement("span");
+    currentDate.textContent =
+        ": " + moment(data.current.dt.value).format("MMM D, YYYY");
+    cityDetailsEL.appendChild(currentDate);
+
+    var weatherIcon = document.createElement("img")
+    var icon = data.current.weather[0].icon;
+    console.log(icon);
+    weatherIcon.setAttribute(
+        "src", 'http://openweathermap.org/img/wn/' + icon + '@2x.png');
+    cityInputEl.appendChild(weatherIcon);
+    console.log(weatherIcon);
+
+    var tempEl = document.createElement("span");
+    tempEl.textContent = "Temperature: " + Math.round(data.current.temp) + " °F";
+    tempEl.classList = "list-group-item";
+    currentWeatherEl.appendChild(tempEl);
+    console.log(data.current.temp);
+
+    var humEl = document.createElement("span");
+    humEl.textContent = "Humidity: " + data.current.humidity + "%";
+    humEl.classList = "list-group-item";
+    currentWeatherEl.appendChild(humEl);
+
+    var windEl = document.createElement("span");
+    windEl.textContent = "Wind Speed: " + data.current.wind_speed + " MPH";
+    windEl.classList = "list-group-item";
+    currentWeatherEl.appendChild(windEl);
+
+    var uvEl = document.createElement("span");
+    uvEl.textContent = "UV: " + data.current.uvi;
+    uvEl.classList = "list-group-item";
+    currentWeatherEl.appendChild(uvEl);
+};
 
 
 // listen for "get forecast click and run the above"
